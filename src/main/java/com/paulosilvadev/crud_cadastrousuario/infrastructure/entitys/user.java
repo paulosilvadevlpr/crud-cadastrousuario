1a3263877bc80027b7978a1a9635dd5b80014721
phdevlpr <phdevlpr@gmail.com>
2025-09-12 19:32:04 -0300

    feat: criando a tabela de usuários usando o @Id para gerar um identificador automático, constução das colunas email, nome e data de nascimento(com LocalData).

---
+++ src/main/java/com/paulosilvadev/crud_cadastrousuario/infrastructure/entitys/user.java
@@ -0,0 +1,30 @@
+package com.paulosilvadev.crud_cadastrousuario.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user")
+@Entity
+
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "birthday")
+    private LocalDate birthDay;
+}
