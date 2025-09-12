58edab8480e6ce460ed0f48d870380830ee8a566
phdevlpr <phdevlpr@gmail.com>
2025-09-12 18:57:27 -0300

    first commit

---
+++ src/main/java/com/paulosilvadev/crud_cadastrousuario/CrudCadastrousuarioApplication.java
@@ -0,0 +1,13 @@
+package com.paulosilvadev.crud_cadastrousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCadastrousuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCadastrousuarioApplication.class, args);
+	}
+
+}
